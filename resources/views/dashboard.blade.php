4e4d71140e39c79d08d9750d1e38b981ee1ae488
danialnbl <danialnabil0208@gmail.com>
2024-08-29 18:46:08 +0800

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Halaman Utama') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-{{--            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">--}}
+{{--            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-2">--}}
 {{--                <div class="p-6 text-gray-900">--}}
 {{--                    {{ __("You're logged in!") }}--}}
 {{--                </div>--}}
-
 {{--            </div>--}}
+{{--            Card Information--}}
+            <div class="grid md:grid-cols-3 sm:grid-cols-1 gap-x-4">
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">TUNTUTAN DALAM PROSES</h5>
+                    <p class="mb-3 font-normal text-green-500 dark:text-green-500">0</p>
+                </div>
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">TUNTUTAN DILULUSKAN</h5>
+                    <p class="mb-3 font-normal text-green-500 dark:text-green-500">0</p>
+                </div>
+                <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-2">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">AHLI KELUARGA</h5>
+                    <p class="mb-3 font-normal text-green-500 dark:text-green-500">0</p>
+                </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,7 +13,17 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Halaman Utama') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('admin.dashboard')">
+                        {{ __('Tuntutan') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('admin.dashboard')">
+                        {{ __('Yuran') }}
                     </x-nav-link>
                 </div>
             </div>
